21f9043e66b4823f6ba1a8cc1ba82c68b5061be7
chandrika31 <udhayachandrikamurugan94@gmail.com>
2022-09-28 23:27:18 +0530

    design changes

--- MurasoliNews/src/app/header/header.component.ts
+++ MurasoliNews/src/app/header/header.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  day: any;
+  dateToday: Date = new Date();
   constructor() { }
 
   ngOnInit(): void {
+    this.day = new Date().toLocaleString('en-us', {weekday:'long'});
   }
 
 }
