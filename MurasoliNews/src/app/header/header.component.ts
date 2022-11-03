d8f8bd4b6f43f3ee5a887c240319a6fcc440b642
chandrika31 <udhayachandrikamurugan94@gmail.com>
2022-11-03 16:03:29 +0530

    header changes

--- MurasoliNews/src/app/header/header.component.ts
+++ MurasoliNews/src/app/header/header.component.ts
@@ -12,6 +12,30 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
     this.day = new Date().toLocaleString('en-us', {weekday:'long'});
+    const dayStr: string = this.day.toString().split(',');
+    switch(dayStr[0].toString().toUpperCase()) {
+      case 'MONDAY':
+        this.day = this.day.toString().replace(dayStr[0], 'திங்கட்கிழமை');
+        break;
+      case 'TUESDAY':
+        this.day = this.day.toString().replace(dayStr[0], 'செவ்வாய் கிழமை');
+        break;
+      case 'WEDNESDAY':
+        this.day = this.day.toString().replace(dayStr[0], 'புதன் கிழமை');
+        break;
+      case 'THURSDAY':
+        this.day = this.day.toString().replace(dayStr[0], 'வியாழக்கிழமை');
+        break;
+      case 'FRIDAY':
+        this.day = this.day.toString().replace(dayStr[0], 'வெள்ளிக்கிழமை');
+        break;
+      case 'SATURDAY':
+        this.day = this.day.toString().replace(dayStr[0], 'சனிக்கிழமை');
+        break;
+      case 'SUNDAY':
+        this.day = this.day.toString().replace(dayStr[0], 'ஞாயிற்றுக்கிழமை');
+        break;
+    }
   }
 
 }
