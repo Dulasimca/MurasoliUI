008745c9660a5e286091559e04eb846126ed8048
chandrika31 <udhayachandrikamurugan94@gmail.com>
2022-11-30 18:03:26 +0530

    added helper class

--- MurasoliNews/src/app/helper/converter.ts
+++ MurasoliNews/src/app/helper/converter.ts
@@ -1,29 +1,71 @@
 export class Converter {
-    day: string = '';
+    value: string = '';
     convert(name: string): string {
-        switch(name) {
+        switch (name) {
             case 'MONDAY':
-              this.day = 'திங்கட்கிழமை';
-              break;
+                this.value = 'திங்கட்கிழமை';
+                break;
             case 'TUESDAY':
-              this.day = 'செவ்வாய் கிழமை';
-              break;
+                this.value = 'செவ்வாய் கிழமை';
+                break;
             case 'WEDNESDAY':
-              this.day = 'புதன் கிழமை';
-              break;
+                this.value = 'புதன் கிழமை';
+                break;
             case 'THURSDAY':
-              this.day = 'வியாழக்கிழமை';
-              break;
+                this.value = 'வியாழக்கிழமை';
+                break;
             case 'FRIDAY':
-              this.day = 'வெள்ளிக்கிழமை';
-              break;
+                this.value = 'வெள்ளிக்கிழமை';
+                break;
             case 'SATURDAY':
-              this.day = 'சனிக்கிழமை';
-              break;
+                this.value = 'சனிக்கிழமை';
+                break;
             case 'SUNDAY':
-              this.day = 'ஞாயிற்றுக்கிழமை';
-              break;
-          }
-          return this.day;
+                this.value = 'ஞாயிற்றுக்கிழமை';
+                break;
+        }
+        return this.value;
+    }
+
+    convertMonth(value: string): string {
+        switch (value) {
+            case 'JAN':
+                this.value = 'ஜனவரி';
+                break;
+            case 'FEB':
+                this.value = 'பிப்ரவரி';
+                break;
+            case 'MAR':
+                this.value = 'மார்ச்';
+                break;
+            case 'APR':
+                this.value = 'ஏப்ரல்';
+                break;
+            case 'MAY':
+                this.value = 'மே';
+                break;
+            case 'JUN':
+                this.value = 'ஜூன்';
+                break;
+            case 'JUL':
+                this.value = 'ஜூலை';
+                break;
+            case 'AUG':
+                this.value = 'ஆகஸ்ட்';
+                break;
+            case 'SEP':
+                this.value = 'செப்டம்பர்';
+                break;
+            case 'OCT':
+                this.value = 'அக்டோபர்';
+                break;
+            case 'NOV':
+                this.value = 'நவம்பர்';
+                break;
+            case 'DEC':
+                this.value = 'டிசம்பர்';
+                break;
+        }
+        return this.value;
     }
 }
--- MurasoliNews/src/app/home/home.component.ts
+++ MurasoliNews/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Converter } from '../helper/converter';
 import { DataSharingService } from '../services/data-sharing.service';
 import { RestapiService } from '../services/restapi.service';
 
@@ -18,7 +19,8 @@ export class HomeComponent implements OnInit {
   breakingNews: string = '';
 
   constructor(private _router: Router, private _restApiService: RestapiService,
-    private _dataSharing: DataSharingService, private _datepipe: DatePipe) { }
+    private _dataSharing: DataSharingService, private _datepipe: DatePipe,
+    private _converter: Converter) { }
 
   ngOnInit(): void {
     this._restApiService.get('FlashNewsEntry/GetFlashNewsEntry').subscribe(res => {
@@ -29,30 +31,42 @@ export class HomeComponent implements OnInit {
     this._restApiService.get('MainNewsEntry/GetMainNewsEntry').subscribe(res => {
       var centerColData: any[] = [];
       res.Table.forEach((data: any) => {
+        var date = this._datepipe.transform(data.g_incidentdate, 'MMM,dd h:mm a');
+        var temp = date!.toString().split(',');
+        var incidentMonth = this._converter.convertMonth(temp[0].toString().toUpperCase());
+        const incidentDate = date?.toString().replace(temp[0], incidentMonth);
         if (data.g_displayside === 0 && data.g_priority === 2) {
           this.firstColData.push(
-            { incidentDate: this._datepipe.transform(data.g_incidentdate, 'MMM,dd h:mm a'), headLine: data.g_newstitletamil,
-             newsShort: data.g_newsshorttamil, newsDetail: data.g_newsdetailstamil }
+            {
+              incidentDate: incidentDate, headLine: data.g_newstitletamil,
+              newsShort: data.g_newsshorttamil, newsDetail: data.g_newsdetailstamil
+            }
           );
-        } else if(data.g_displayside === 2 && data.g_priority === 2) {
+        } else if (data.g_displayside === 2 && data.g_priority === 2) {
           centerColData.push(
-            { incidentDate: this._datepipe.transform(data.g_incidentdate, 'MMM,dd h:mm a'), headLine: data.g_newstitletamil,
-             newsShort: data.g_newsshorttamil, newsDetail: data.g_newsdetailstamil }
+            {
+              incidentDate: incidentDate, headLine: data.g_newstitletamil,
+              newsShort: data.g_newsshorttamil, newsDetail: data.g_newsdetailstamil
+            }
           );
-        } else if(data.g_displayside === 1 && data.g_priority === 2) {
+        } else if (data.g_displayside === 1 && data.g_priority === 2) {
           this.thridColData.push(
-            { incidentDate: this._datepipe.transform(data.g_incidentdate, 'MMM,dd h:mm a'), headLine: data.g_newstitletamil,
-             newsShort: data.g_newsshorttamil, newsDetail: data.g_newsdetailstamil }
+            {
+              incidentDate: incidentDate, headLine: data.g_newstitletamil,
+              newsShort: data.g_newsshorttamil, newsDetail: data.g_newsdetailstamil
+            }
           );
-        } else if(data.g_displayside === 3 && data.g_priority === 2) {
+        } else if (data.g_displayside === 3 && data.g_priority === 2) {
           this.bottomNewsData.push(
-            { incidentDate: this._datepipe.transform(data.g_incidentdate, 'MMM,dd h:mm a'), headLine: data.g_newstitletamil,
-             newsShort: data.g_newsshorttamil, newsDetail: data.g_newsdetailstamil }
+            {
+              incidentDate: incidentDate, headLine: data.g_newstitletamil,
+              newsShort: data.g_newsshorttamil, newsDetail: data.g_newsdetailstamil
+            }
           );
         }
       })
       centerColData.forEach((item: any, index: number) => {
-        if(index != 0) {
+        if (index != 0) {
           this.secondColData.push(item);
         } else {
           this.secondMainColData.push(item);
