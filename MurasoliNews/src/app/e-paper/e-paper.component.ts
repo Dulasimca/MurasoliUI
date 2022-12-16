f89d057433a11f2bbfd35f3c8f16c68cba9254d5
chandrika31 <udhayachandrikamurugan94@gmail.com>
2022-12-16 16:55:31 +0530

    e-paper changes

--- MurasoliNews/src/app/e-paper/e-paper.component.ts
+++ MurasoliNews/src/app/e-paper/e-paper.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SelectItem } from 'primeng/api';
-import { NgxExtendedPdfViewerService, pdfDefaultOptions } from 'ngx-extended-pdf-viewer';
+import { NgxExtendedPdfViewerService, pdfDefaultOptions, TextLayerRenderedEvent } from 'ngx-extended-pdf-viewer';
 import { DataSharingService } from '../services/data-sharing.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-e-paper',
@@ -11,14 +12,31 @@ import { DataSharingService } from '../services/data-sharing.service';
 export class EPaperComponent implements OnInit {
   src: any;
   districtOptions: SelectItem[] = [];
-  district: any;
+  district: any = 0;
   date: Date = new Date();
   items: any = [];
-  constructor(private _dataSharing: DataSharingService) {
+  layers: any;
+  public enablePinchOnMobile = true;
+
+  public zoomLevels = ['auto', 'page-actual', 'page-fit', 'page-width',
+    0.5, 0.67, 0.75, 0.82, 0.9, 1, 1.1, 1.15,
+    1.25, 1.5];
+  urlSafe: any;
+   
+  constructor(private _dataSharing: DataSharingService, private _pdfService: NgxExtendedPdfViewerService,
+    private sanitizer: DomSanitizer) {
     pdfDefaultOptions.doubleTapZoomFactor = '150%'; // The default value is '200%'
     pdfDefaultOptions.maxCanvasPixels = 4096 * 4096 * 5;
   }
 
+  public exportAsText(pageNum: number): void {
+    (async () => {
+      const text = await this._pdfService.getPageAsText(pageNum);
+      console.log('txt', text);
+    })();
+  }
+  
+  pageChange($event: any) { console.log('anno eve', $event ); this.exportAsText($event); };
   ngOnInit(): void {
     this.src = 'assets/files/murasoliE-paper.pdf';
     this.districtOptions = [
@@ -26,6 +44,8 @@ export class EPaperComponent implements OnInit {
       { label: 'கோயம்புத்தூர்', value: '1' },
       { label: 'மதுரை', value: '2' },
     ];
+    var url: string = this._dataSharing.fileURL +  "sample.indd";
+    this.urlSafe= this.sanitizer.bypassSecurityTrustResourceUrl(url);
 
     this.items = [
       {
@@ -56,6 +76,15 @@ export class EPaperComponent implements OnInit {
   }
 
   onSelectDistrict() { }
+
+  highlightWords($event: any) {
+    console.log('event', $event);
+    $event.source.enhanceTextSelection = true;
+  }
+
+  getSelect($event: any) {
+    console.log('select', $event)
+  }
 
   share(type: string) {
     const shareUrl = this._dataSharing.fileURL + 'murasoliE-paper.pdf';
